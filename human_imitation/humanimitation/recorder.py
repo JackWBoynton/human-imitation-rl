6adc83853d9b1fa14e6133fdd48d74657c2a5774
Jack Boynton <jackboynton@Jacks-MacBook-Pro-4.local>
2022-01-23 13:21:34 -0500

    updated comments and added custom loss calculations

--- human_imitation/humanimitation/recorder.py
+++ human_imitation/humanimitation/recorder.py
@@ -30,30 +30,26 @@ class ScreenRecorderThread(threading.Thread):
         self.running = False
 
 
-def aggregate(frames_loc="./frames/*", inputs_loc="inputs/input_recording_end_1642457354223460000.pkl", output_loc="output"):
+def aggregate(frames_loc="frames", inputs_loc="inputs/input_recording_end_1642457354223460000.pkl", output_loc="output", loss_fns=None):
     # maps input record at near continuous FPS to the "slower" FPS of the screen recording so each frame from the screen recording has respective inputs
-    frames = glob.glob(frames_loc)
-    print(frames)
+
+    assert os.path.exists(input_loc), "must specify keyboard pickle input file"
+    assert os.path.exists(frames_loc), "must specify frames pickle input file"
+
+    frames = glob.glob(f"{frames_loc}/*")
     lframes = np.zeros((len(frames), 410, 745, 3))
     lframes_ts = np.zeros((len(frames), 1))
     for n, frame in enumerate(frames):
         with open(frame, 'rb') as handle:
             b = pickle.load(handle)
-            print(b[1].shape)
             lframes[n] = b[1]
             lframes_ts[n] = b[0] 
+
+    out = sorted(list(zip(lframes, lframes_ts)), key=lambda x: int(x[1])) # sort by frame timestamp
+
     
-    # zz = list(zip(lframes, lframes_ts))
-    # first = min(zz, key=lambda x: int(x[1]))[1]
-    # last = max(zz, key=lambda x: int(x[1]))[1]
-
-    out = sorted(list(zip(lframes, lframes_ts)), key=lambda x: int(x[1]))
-
-
     with open(inputs_loc, "rb") as f:
         inputs = pickle.load(f)
-
-    print(inputs)
 
     act2frame = [None]*len(frames)
 
@@ -67,35 +63,48 @@ def aggregate(frames_loc="./frames/*", inputs_loc="inputs/input_recording_end_16
                             act2frame[m] = [*act2frame[m], input_]
                         else: act2frame[m] = input_
     
-    
 
-    loss = [TimeLoss(template_directory="../../../mariokart/seven_segment_matching/time"), LapLoss(template_directory="../../../mariokart/seven_segment_matching/lap")]
+    if loss_fns is not None:
+        ll = lambda x: [round(l.eval(x), 5) for l in loss_fns]
 
-    ll = lambda x: [round(l.eval(x), 5) for l in loss]
+        losses = []
+        for frame, ts in out:
+            frame = frame.astype(np.uint8)
+            losses.append(sum(ll(frame)))
+        a, b = np.array(out)[:, 0], np.array(out)[:, 1]
+        total = np.asarray(list(zip(a, b, losses, act2frame)))
+        # frame, timestamp, loss, actions
 
+        # stable baselines format
+        total_dict = {"acs": total[:, 3], "rews": -total[:, 2], "obs": total[:, 0], "et_rets": np.sum(-total[:, 2])}
 
-    losses = []
-    for frame, ts in out:
-        frame = frame.astype(np.uint8)
-        losses.append(sum(ll(frame)))
-    a, b = np.array(out)[:, 0], np.array(out)[:, 1]
-    total = np.asarray(list(zip(a, b, losses)))
+        if not os.path.exists(output_loc):
+            os.makedirs(output_loc)
+
+        with open(f"{output_loc}/final.pkl", "wb") as f:
+            pickle.dump(total_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+        return total_dict
+    # only observations and actions
+    total_dict = {"acs": total[:, 3], "obs": total[:, 0]}
 
     if not os.path.exists(output_loc):
         os.makedirs(output_loc)
 
     with open(f"{output_loc}/final.pkl", "wb") as f:
-        pickle.dump(total, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-    return total
+        pickle.dump(total_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
         
     
+def test_imitation(total_dict_loc="output/final.pkl"):
+    with open(f"{total_dict_loc}/final.pkl", "rb") as f:
+        traj = pickle.load(f)
+
     
 
 class Recorder:
     def __init__(self):
         self.inputrec = InputRecorder(
-            ["a", "x", "q", "left", "right"])  # has own threading
+            ["a", "x", "q", "left", "right"])  # only record these keystrokes
         self.screenrec = ScreenRecorderThread()
 
     def stop(self):
@@ -111,7 +120,7 @@ class Recorder:
 
 
 if __name__ == "__main__":
-    aggregate()
+    aggregate(loss_fns=[TimeLoss(template_directory="../../../mariokart/seven_segment_matching/time"), LapLoss(template_directory="../../../mariokart/seven_segment_matching/lap")])
     # rec = Recorder()
     # rec.start()
     # try:
